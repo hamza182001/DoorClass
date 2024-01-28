7cc687b0c17df9455a08988c5d6a29c7745a2ef2
Mir <mir@gmail.com>
2024-01-28 21:06:59 +0500

    Door Unlocked Implemented

--- Door.cpp
+++ Door.cpp
@@ -14,7 +14,12 @@ public:
 		isOpen = false;
 	};
 	void lockDoor();
-	void unlock();
+
+    void unlock() {
+        islocked = false;
+        cout << "Door is unlocked.\n";
+    }
+
 	bool isDoorOpen() const{
 		if(isOpen == true){
 			return true;
